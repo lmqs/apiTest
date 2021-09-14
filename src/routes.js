863129d706fd3b7b442076c086b4acb56eeaf121
Luciana <lucianaqueiroz098@hotmail.com>
2021-09-14 20:42:15 -0300

    teste

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const ApiController = require('./controllers/ApiController');
 
 router.get('/ping', ApiController.ping);
-router.get('/proventos/:tipo/:itensPagina/:page' , ApiController.proventos);
-router.get('/relatorios/:tipo/:itensPagina/:page' , ApiController.relatorios);
-router.get('/ipos/:tipo/:itensPagina/:page' , ApiController.IPOs);
-router.get('/todos/:itensPagina/:page' , ApiController.verTodos);
+// router.get('/proventos/:tipo/:itensPagina/:page' , ApiController.proventos);
+// router.get('/relatorios/:tipo/:itensPagina/:page' , ApiController.relatorios);
+// router.get('/ipos/:tipo/:itensPagina/:page' , ApiController.IPOs);
+// router.get('/todos/:itensPagina/:page' , ApiController.verTodos);
 
 
-router.get('/indicesAtual' , ApiController.indicesAtual);
-router.get('/geral' , ApiController.geral);
+// router.get('/indicesAtual' , ApiController.indicesAtual);
+// router.get('/geral' , ApiController.geral);
 
 
 
